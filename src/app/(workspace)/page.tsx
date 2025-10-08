f29fdeb3bc78f4ec00b64f81cd2a72357addf45e
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-10-08 08:56:21 +0530

    04_Header

--- src/app/(workspace)/page.tsx
+++ src/app/(workspace)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { currentUser } from "@/modules/authentication/actions";
 import UserButton from "@/modules/authentication/components/user-button";
 export default async function Home() {
-  const user = await currentUser
+  const user = await currentUser()
   return (
   
    <div className="flex flex-col items-center justify-center">
---
+++ src/modules/layout/components/invite-member.tsx
@@ -0,0 +1,18 @@
+import { Hint } from '@/components/ui/hint'
+import { Button } from '@/components/ui/button'
+import { UserPlus } from 'lucide-react'
+import React from 'react'
+
+const InviteMember = () => {
+  return (
+    <Hint label='Invite Members'>
+    <div>
+         <Button className="border border-emerald-400 bg-emerald-400/10 hover:bg-emerald-400/20 text-emerald-400 hover:text-emerald-300">
+            <UserPlus className="size-4 text-emerald-400" />
+          </Button>
+    </div>
+     </Hint>
+  )
+}
+
+export default InviteMember
